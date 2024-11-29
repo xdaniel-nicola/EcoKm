1cda402f3f0e2539e863c3ce76313738e5dcd9d5
Daniel Nicola <daniel.nicola02@gmail.com>
2024-11-29 20:33:24 -0300

    mudanças para mostrar as viagens e estilizar o perfil no geral

--- usuario/perfil.php
+++ usuario/perfil.php
@@ -84,127 +84,128 @@ if (!isset($_SESSION['username'])) {
                 }
                 echo '</table>';
             echo '</div>';
+            $sqlPlano = "SELECT * FROM plano WHERE cpf = :cpf";
+            $stmtPlano = $pdo->prepare($sqlPlano);
+            $stmtPlano->bindValue(':cpf', $usuario['cpf'], PDO::PARAM_STR);
+            $stmtPlano->execute();
+            $planos = $stmtPlano->fetchAll(PDO::FETCH_ASSOC);
+            
+            echo '<div class="planosAtivos">';
+            echo '<h2>Plano Ativo</h2>';
+            
+            if ($planos) {
+                echo '<table class="table table-striped">';
+                echo '<thead>';
+                echo '<tr>';
+                echo '<th>Plano</th>';
+                echo '<th>Preço (R$)</th>';
+                echo '<th></th>';
+                echo '</tr>';
+                echo '</thead>';
+                echo '<tbody>';
+                foreach ($planos as $plano) {
+                    echo '<tr>';
+                    echo '<td>' . htmlspecialchars($plano['tipo']) . '</td>';
+                    echo '<td>' . htmlspecialchars($plano['preco']) . '</td>';
+                    echo '<td>
+                            <form method="POST" action="perfil.php" id="formCancelamento">
+                                <input type="hidden" name="id_plano" value="' . htmlspecialchars($plano['id_plano']) . '">
+                                <button type="button" class="cancelaPlano" id="cancelarBtn">Cancelar plano</button>
+                            </form>
+                            </td>';
+                    echo '</tr>';
+                }
+                echo '</tbody>';
+                echo '</table>';
+                
+                echo '</div>';
+                    } else {
+                        echo '<p>Não há plano.</p>';
+                        echo '<a class="alterBtn" href="../planos/planos.php">Assinar plano</a>';
+                    }
 
-                $sqlCarro = "SELECT * FROM carro WHERE cpf = :cpf";
-                $stmtCarro = $pdo->prepare($sqlCarro);
-                $stmtCarro->bindValue(':cpf', $usuario['cpf'], PDO::PARAM_STR);
-                $stmtCarro->execute();
-                $carros = $stmtCarro->fetchAll(PDO::FETCH_ASSOC);
+                    } else {
+                        echo '<p>Nenhum resultado encontrado.</p>';
+                    }
+                } else {
+                    echo '<p>Erro na conexão com o banco de dados.</p>';
+                }
+                if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id_plano'])) {
+                    $idPlano = $_POST['id_plano'];
+                
+                
+                    if ($pdo) {
+                        $sqlCancel = "UPDATE plano SET cpf = NULL WHERE id_plano = :id_plano";
+                        $stmtCancel = $pdo->prepare($sqlCancel);
+                        $stmtCancel->bindValue(':id_plano', $idPlano, PDO::PARAM_INT);
+                        $stmtCancel->execute();
+                
+                        header("Location: perfil.php?msg=Plano cancelado com sucesso");
+                        exit();
+                    }
+                }
+                echo '</div>';
 
-                echo '<div class="carroCadastro">';
-                echo '<h2>Carros Cadastrados</h2>';
 
-                if ($carros) {
+                $sqlViagem = "SELECT * FROM viagem WHERE cpf = :cpf";
+                $stmtViagem = $pdo->prepare($sqlViagem);
+                $stmtViagem->bindValue(':cpf', $usuario['cpf'], PDO::PARAM_STR);
+                $stmtViagem->execute();
+                $viagens = $stmtViagem->fetchAll(PDO::FETCH_ASSOC);
+
+                echo '<div class="viagemSalva">';
+                echo '<h2>Viagens Salvas</h2>';
+
+                if ($viagens) {
                     echo '<div class="table-responsive">';
                     echo '<table class="table table-striped">';
                     echo '<thead>';
                     echo '<tr>';
-                    echo '<th>Id</th>';
-                    echo '<th>Modelo</th>';
-                    echo '<th>Marca</th>';
-                    echo '<th>Motor</th>';
+                    // echo '<th>Id</th>';
+                    echo '<th>Tipo de Motor</th>';
+                    echo '<th>Tipo de Combustível</th>';
+                    echo '<th>Distância</th>';
+                    echo '<th>Preço do Combustível</th>';
+                    echo '<th>Partida</th>';
+                    echo '<th>Chegada</th>';
+                    echo '<th>Tempo Estimado</th>';
+                    echo '<th>Consumo</th>';
+                    echo '<th>Custo</th>';
                     echo '<th></th>';
                     echo '</tr>';
                     echo '</thead>';
                     
                     echo '<tbody>';
-                    foreach ($carros as $carro) {
+                    foreach ($viagens as $viagem) {
                         echo '<tr>';
-                        echo '<td>' . htmlspecialchars($carro['id_carro']) . '</td>';
-                        echo '<td>' . htmlspecialchars($carro['modelo']) . '</td>';
-                        echo '<td>' . htmlspecialchars($carro['marca']) . '</td>';
-                        echo '<td>' . htmlspecialchars($carro['motor']) . '</td>';
-                        echo '<td><a class="alterBtn" href="atualizarCarro.php?id= '.$carro["id_carro"].'">Atualizar carro</a>
-                        <a class="alterBtn" href="apagarCarro.php?id= '.$carro["id_carro"].'">Apagar carro</a></td>';
+                        // echo '<td>' . htmlspecialchars($viagem['id_viagem'] ?? 'Não informado') . '</td>';
+                        echo '<td>' . htmlspecialchars($viagem['tipoMotor'] ?? 'Não informado') . '</td>';
+                        echo '<td>' . htmlspecialchars($viagem['tipoCombustivel'] ?? 'Não informado') . '</td>';
+                        echo '<td>' . htmlspecialchars($viagem['distancia'] ?? 'Não informado') . 'km' . '</td>';
+                        echo '<td>'. 'R$' . htmlspecialchars($viagem['precoCombustivel'] ?? 'Não informado') . '</td>';
+                        echo '<td>' . htmlspecialchars($viagem['partida'] ?? 'Não informado') . '</td>';
+                        echo '<td>' . htmlspecialchars($viagem['chegada'] ?? 'Não informado') . '</td>';
+                        echo '<td>' . htmlspecialchars($viagem['tempoEstimado'] ?? 'Não informado') . '</td>';
+                        echo '<td>' . htmlspecialchars($viagem['consumo'] ?? 'Não informado') . 'L' . '</td>';
+                        echo '<td>'. 'R$' . htmlspecialchars($viagem['custo'] ?? 'Não informado') . '</td>';
+                        echo '<td><a class="alterBtn" href="apagarViagem.php?id= '.$viagem["id_viagem"].'">Apagar viagem</a></td>';
                         echo '</tr>';
                     }
-
-                   
-                    echo '<tr>';
-                    echo '<td colspan="4"></td>'; 
-                    echo '<td><a class="alterBtn" href="cadastroCarro.php">Adicionar Carros</a></td>';
-                    echo '</tr>';
-
-                    echo '</tbody>';
-                    echo '</table>';
-                    echo '</div>';
                 } else {
-                    echo '<p>Não há carros cadastrados.</p>';
-                    echo '<a class="alterBtn" href="cadastroCarro.php">Adicionar Carros</a>';
+                    echo '<p>Não há viagens salvas.</p>';
+                    echo '<a class="alterBtn" href="../index/php">Fazer viagem</a>';
                 }
 
                 echo '</div>';
-                
-                $sqlPlano = "SELECT * FROM plano WHERE cpf = :cpf";
-                $stmtPlano = $pdo->prepare($sqlPlano);
-                $stmtPlano->bindValue(':cpf', $usuario['cpf'], PDO::PARAM_STR);
-                $stmtPlano->execute();
-                $planos = $stmtPlano->fetchAll(PDO::FETCH_ASSOC);
-                
-                echo '<div class="planosAtivos">';
-                echo '<h2>Plano Ativo</h2>';
-                
-                if ($planos) {
-                    echo '<table class="table table-striped">';
-                    echo '<thead>';
-                    echo '<tr>';
-                    echo '<th>Plano</th>';
-                    echo '<th>Preço (R$)</th>';
-                    echo '<th></th>';
-                    echo '</tr>';
-                    echo '</thead>';
-                    echo '<tbody>';
-                    foreach ($planos as $plano) {
-                        echo '<tr>';
-                        echo '<td>' . htmlspecialchars($plano['tipo']) . '</td>';
-                        echo '<td>' . htmlspecialchars($plano['preco']) . '</td>';
-                        echo '<td>
-                                <form method="POST" action="perfil.php" id="formCancelamento">
-                                    <input type="hidden" name="id_plano" value="' . htmlspecialchars($plano['id_plano']) . '">
-                                    <button type="button" class="cancelaPlano" id="cancelarBtn">Cancelar plano</button>
-                                </form>
-                                </td>';
-                        echo '</tr>';
-                    }
-                    echo '</tbody>';
-                    echo '</table>';
-                    
-                    echo '</div>';
-            echo '</div>';
-                } else {
-                    echo '<p>Não há plano.</p>';
-                    echo '<a class="alterBtn" href="../planos/planos.php">Assinar plano</a>';
-                }
-
-            } else {
-                echo '<p>Nenhum resultado encontrado.</p>';
-            }
-        } else {
-            echo '<p>Erro na conexão com o banco de dados.</p>';
-        }
-        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id_plano'])) {
-            $idPlano = $_POST['id_plano'];
-        
-        
-            if ($pdo) {
-                $sqlCancel = "UPDATE plano SET cpf = NULL WHERE id_plano = :id_plano";
-                $stmtCancel = $pdo->prepare($sqlCancel);
-                $stmtCancel->bindValue(':id_plano', $idPlano, PDO::PARAM_INT);
-                $stmtCancel->execute();
-        
-                header("Location: perfil.php?msg=Plano cancelado com sucesso");
-                exit();
-            }
-        }
-        ?>
+                ?>
         <div id="modalCancelar" class="modal">
     <div class="modal-content">
         <span class="close" id="closeModal">&times;</span>
         <h2>Tem certeza que deseja cancelar o plano?</h2>
         <form id="formCancelamento" method="POST" action="perfil.php">
             <input type="hidden" name="id_plano" id="id_plano_modal">
-            <button type="button" id="simCancelarBtn">Sim, cancelar</button>
-            <button type="button" id="voltarBtn">Voltar</button>
+            <button class="modalBtn" type="button" id="simCancelarBtn">Sim, cancelar</button>
+            <button class="modalBtn" type="button" id="voltarBtn">Voltar</button>
         </form>
     </div>
 </div>
