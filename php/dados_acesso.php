060cccfdf9ce0c6da5de17f84072377632b024a3
xdaniel-nicola <daniel.nicola02@gmail.com>
2025-06-11 21:41:01 -0300

    dados aws para o banco de dados

--- php/dados_acesso.php
+++ php/dados_acesso.php
@@ -1,7 +1,7 @@
 <?php
    define("DSN", "mysql");
-   define("SERVIDOR", "localhost");
-   define("USUARIO", "root");
-   define("SENHA", "1354");
+   define("SERVIDOR", "database-2.cnjffauvqcgb.us-east-1.rds.amazonaws.com");
+   define("USUARIO", "admin");
+   define("SENHA", "admin#12345");
    define("BANCODEDADOS", "ecokm");
 ?>
