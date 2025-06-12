36306f5792ad6735a041d2a1305a20b2fcb9a2a4
xdaniel-nicola <daniel.nicola02@gmail.com>
2025-06-11 21:41:59 -0300

    dados aws para o banco

--- php/dados_acesso.php
+++ php/dados_acesso.php
@@ -5,3 +5,10 @@
    define("SENHA", "admin#12345");
    define("BANCODEDADOS", "ecokm");
 ?>
+
+<!-- 
+   define("DSN", "mysql");
+   define("SERVIDOR", "localhost");
+   define("USUARIO", "root");
+   define("SENHA", "1354");
+   define("BANCODEDADOS", "ecokm"); -->
