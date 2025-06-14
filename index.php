11b975b905311ea50e67e9a83d24908dfed6867b
xdaniel-nicola <daniel.nicola02@gmail.com>
2025-06-14 13:23:44 -0300

    debug

--- index.php
+++ index.php
@@ -3,6 +3,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+echo $variavel_inexistente;
+
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
