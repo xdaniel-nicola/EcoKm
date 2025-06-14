2b720be07627fc791b833f11012d67bfea259882
xdaniel-nicola <daniel.nicola02@gmail.com>
2025-06-14 13:56:24 -0300

    debug

--- php/dados_acesso.php
+++ php/dados_acesso.php
@@ -6,9 +6,9 @@
    define("BANCODEDADOS", "ecokm");
 ?>
 
-<!-- 
-   define("DSN", "mysql");
-   define("SERVIDOR", "localhost");
-   define("USUARIO", "root");
-   define("SENHA", "1354");
-   define("BANCODEDADOS", "ecokm"); -->
+ 
+<!-- #   define("DSN", "mysql"); -->
+<!-- #  define("SERVIDOR", "localhost"); -->
+<!-- #   define("USUARIO", "root"); -->
+<!-- #   define("SENHA", "1354"); -->
+<!-- #   define("BANCODEDADOS", "ecokm"); -->
